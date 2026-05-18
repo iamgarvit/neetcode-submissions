c8d5f7a8256a8298e837d7294365ce1a0555c944
Garvit <87112688+iamgarvit@users.noreply.github.com>
2026-05-18 17:22:31 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c: s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+            else {
+                if (c == ')' && !st.empty() && st.top() == '(') {
+                    st.pop();
+                }
+                else if (c == ']' && !st.empty() && st.top() == '[') {
+                    st.pop();
+                }
+                else if (c == '}' && !st.empty() && st.top() == '{') {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if (st.empty()) return true;
+        return false;
+    }
+};
