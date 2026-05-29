aef3c70df326b41ff3fd8f0624ec2e7aec59981d
Garvit <87112688+iamgarvit@users.noreply.github.com>
2026-05-29 23:32:26 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size(), sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 == 1)   return false;
+        int target = sum / 2;
+
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+                int notTake = dp[i - 1][j];
+
+                int take = 0;
+                if (nums[i] <= j) {
+                    take = dp[i - 1][j - nums[i]];
+                }
+                dp[i][j] = take || notTake;
+            }
+        }
+        return dp[n - 1][target] == 0 ? false : true;
+    }
+};
